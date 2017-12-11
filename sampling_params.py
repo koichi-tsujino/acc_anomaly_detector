c0dc52386895156c3d47465b8c3b70697d49dcd2
Koichi Tsujino <koichi_tsujino@kenlex.com>
2017-12-11 14:15:35 +0900

    Befor using sampling_params

---
+++ sampling_params.py
@@ -0,0 +1,28 @@
+# ----------------------------------------------------------------------
+# Numenta Platform for Intelligent Computing (NuPIC)
+# Copyright (C) 2013, Numenta, Inc.  Unless you have an agreement
+# with Numenta, Inc., for a separate license for this software code, the
+# following terms and conditions apply:
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU Affero Public License for more details.
+#
+# You should have received a copy of the GNU Affero Public License
+# along with this program.  If not, see http://www.gnu.org/licenses.
+#
+# http://numenta.org/licenses/
+# ----------------------------------------------------------------------
+
+SAMPLING_PARAMS = {
+'NO_OF_FIFO_BUFFER' : 16,
+'BANDWIDTH_RATE' : 200, # Hz
+'SAMPLING_CYCLE' : 0.05, # seconds
+'MONITOR_TIME' : 120, # seconds
+'SABSAMPLING_RATE' : 10
+}    
